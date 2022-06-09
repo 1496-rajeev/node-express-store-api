35cfa5fb7097c836b7129487e078ef0174a6147b
1496-rajeev <1496piku@gmail.com>
2022-06-09 10:42:51 +0530

    database population code added

--- populate.js
+++ populate.js
@@ -6,10 +6,16 @@ const ProductsJson = require('./products.json');
 const Products = require('./models/products');
 
 const start = async () => {
-  await connectDB(process.env.MONGO_URL);
-  await Products.deleteMany();
-  await Products.create(ProductsJson);
-  console.log('success');
+  try {
+    await connectDB(process.env.MONGO_URL);
+    await Products.deleteMany();
+    await Products.create(ProductsJson);
+    console.log('success');
+    process.exit();
+  } catch (error) {
+    console.error('error');
+    process.exit();
+  }
 };
 
 start();
